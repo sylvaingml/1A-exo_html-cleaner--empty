a973b2ea0a0dc48725d404f3fa763cc644b2302a
sylvaingml <code@sylvaingamel.fr>
2025-01-27 01:07:47 +0100

    Clarify question and bonus questions

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 1A-exo_html-cleaner--empty
 
 [Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/sylvaingml/1A-exo_html-cleaner--empty)
+
+This small project intend to test RegExp ability.
+It also include bugs to be fixed and tasks to complete if extra time is available.
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
-/* TODO:
-   Once transformation is implemented, could you propose an alternative
-   implementation so we remove the "cleanup" button and transformation is
-   applied live on the content of the text area.
-
-   Expose possible options and explain how you ensure performances are
-   not impacted.
- */
 @Component({
   selector: 'app-root',
   templateUrl: 'main.html',
